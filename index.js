1e9c539022b74c76bde98e6c02c5e257423763e1
Đeneta Velispahić <asasad204@gmail.com>
2024-05-14 23:15:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,606 @@
+var pitanja = [
+    [{
+        "pitanje": "Ko se smatra 'Ocem kompjuterskih nauka'?",
+        "a": "Alan Turing",
+        "b": "Bill Gates",
+        "c": "Steve Jobs",
+        "d": "Charles Babbage",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koji je glavni grad Francuske?",
+        "a": "Berlin",
+        "b": "Madrid",
+        "c": "Pariz",
+        "d": "Rim",
+        "odgovor": "c"
+    }, {
+        "pitanje": "Koji je najveći ocean na Zemlji?",
+        "a": "Atlantski ocean",
+        "b": "Indijski ocean",
+        "c": "Južni ocean",
+        "d": "Tihi ocean",
+        "odgovor": "d"
+    }, {
+        "pitanje": "Koji je glavni grad Australije?",
+        "a": "Sydney",
+        "b": "Melbourne",
+        "c": "Canberra",
+        "d": "Brisbane",
+        "odgovor": "c"
+    }],
+    [{
+        "pitanje": "Ko je razvio teoriju relativiteta?",
+        "a": "Isaac Newton",
+        "b": "Albert Einstein",
+        "c": "Galileo Galilei",
+        "d": "Stephen Hawking",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Koja zemlja nosi naziv 'Zemlja izlazećeg sunca'?",
+        "a": "Kina",
+        "b": "Japan",
+        "c": "Južna Koreja",
+        "d": "Vijetnam",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Koja je najveća pustinja na svijetu?",
+        "a": "Sahara",
+        "b": "Gobi",
+        "c": "Arapska",
+        "d": "Antarktička",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja je hemijska oznaka za zlato?",
+        "a": "Au",
+        "b": "Ag",
+        "c": "Fe",
+        "d": "Cu",
+        "odgovor": "a"
+    }],
+    [{
+        "pitanje": "Koje hemijsko jedinjenje predstavlja vodu?",
+        "a": "NaCl",
+        "b": "H2O",
+        "c": "CO2",
+        "d": "O2",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Koja biljka vrši fotosintezu?",
+        "a": "Gljive",
+        "b": "Bakterije",
+        "c": "Alge",
+        "d": "Mahovine",
+        "odgovor": "c"
+    }, {
+        "pitanje": "Koja jedinica se koristi za mjerenje udaljenosti?",
+        "a": "Sekunda",
+        "b": "Metar",
+        "c": "Kilogram",
+        "d": "Watt",
+        "odgovor": "b"
+    }, {"pitanje": "Koja sila uzrokuje tijelo da padne prema Zemlji?",
+    "a": "Elektromagnetska sila",
+    "b": "Gravitaciona sila",
+    "c": "Električna sila",
+    "d": "Magnetična sila",
+    "odgovor": "b"
+    }],
+    [{
+        "pitanje": "Koja je funkcija srca u ljudskom tijelu?",
+        "a": "Pumpanje krvi",
+        "b": "Varenje hrane",
+        "c": "Disanje",
+        "d": "Stvaranje antitijela",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja hemijska oznaka označava kisik?",
+        "a": "O2",
+        "b": "H2O",
+        "c": "CO2",
+        "d": "O",
+        "odgovor": "d"
+    }, {
+        "pitanje": "Šta mjeri termometar?",
+        "a": "Vrijeme",
+        "b": "Temperaturu",
+        "c": "Pritisak",
+        "d": "Vlažnost",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Koliko je 5 puta 8?",
+        "a": "10",
+        "b": "13",
+        "c": "40",
+        "d": "64",
+        "odgovor": "c"
+    }],
+    [{
+        "pitanje": "Koja je funkcija mitohondrija u ćeliji?",
+        "a": "Fotosinteza",
+        "b": "Proizvodnja energije",
+        "c": "Sinteza proteina",
+        "d": "Detoksikacija",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Koja hemijska supstanca čini veći dio atmosfere?",
+        "a": "Kiseonik",
+        "b": "Azot",
+        "c": "Ugljendioksid",
+        "d": "Hidrogen",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Koja jedinica se koristi za mjerenje električnog otpora?",
+        "a": "Volt",
+        "b": "Amper",
+        "c": "Ohm",
+        "d": "Watt",
+        "odgovor": "c"
+    }, {
+        "pitanje": "Ako je x = 3, y = 5, koliko iznosi x² + 2xy - y²?",
+        "a": "0",
+        "b": "16",
+        "c": "14",
+        "d": "4",
+        "odgovor": "c"
+    }],
+    [{
+        "pitanje": "Koja vrsta biomolekula čini enzime?",
+        "a": "Lipidi",
+        "b": "Proteini",
+        "c": "Nukleinske kiseline",
+        "d": "Ugljikohidrati",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Šta predstavlja drugi Newtonov zakon?",
+        "a": "Brzina svjetlosti",
+        "b": "Zakon očuvanja energije",
+        "c": "Sila masi daje ubrzanje",
+        "d": "Zakon gravitacije",
+        "odgovor": "c"
+    }, {
+        "pitanje": "Koja rijeka teče kroz Pariz?",
+        "a": "Rijeka Temza",
+        "b": "Rijeka Sena",
+        "c": "Rijeka Dunav",
+        "d": "Rijeka Tisa",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Ako je log₃(x) = 2, koliko iznosi x?",
+        "a": "3",
+        "b": "6",
+        "c": "9",
+        "d": "12",
+        "odgovor": "c"
+    }],
+    [{
+        "pitanje": "Koja je funkcija DNK u ćeliji?",
+        "a": "Proizvodnja energije",
+        "b": "Prenos genetskih informacija",
+        "c": "Sinteza proteina",
+        "d": "Detoksikacija",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Kako se naziva promjena u frekvenciji zvuka ili svjetlosti zbog relativnog kretanja između izvora i promatrača?",
+        "a": "Dopplerski efekat",
+        "b": "Boyleov zakon",
+        "c": "Ohmov zakon",
+        "d": "Archimedesov princip",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja zemlja se nalazi na Apeninskom poluotoku?",
+        "a": "Francuska",
+        "b": "Italija",
+        "c": "Španija",
+        "d": "Grčka",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Koliko iznosi kvadratni korijen od 144?",
+        "a": "10",
+        "b": "12",
+        "c": "14",
+        "d": "16",
+        "odgovor": "b"
+    }],
+    [{
+        "pitanje": "Koja je funkcija mitohondrija u ćeliji?",
+        "a": "Fotosinteza",
+        "b": "Proizvodnja energije",
+        "c": "Sinteza proteina",
+        "d": "Detoksikacija",
+        "odgovor": "b"
+    }, {
+        "pitanje": "U kojoj je zemlji Veliki koraljni greben?",
+        "a": "Australija",
+        "b": "Brazil",
+        "c": "Meksiko",
+        "d": "Tajland",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja zemlja ima najviše jezera na svijetu?",
+        "a": "SAD",
+        "b": "Rusija",
+        "c": "Kanada",
+        "d": "Australija",
+        "odgovor": "c"
+    }, {
+        "pitanje": "Koja je suma prvih deset prirodnih brojeva?",
+        "a": "45",
+        "b": "50",
+        "c": "55",
+        "d": "60",
+        "odgovor": "c"
+    }],
+    [{
+        "pitanje": "Koja vrsta veze drži atome zajedno u molekulama vode?",
+        "a": "Kovalentna veza",
+        "b": "Ionska veza",
+        "c": "Metalna veza",
+        "d": "Van der Waalsova veza",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja jedinica se koristi za mjerenje brzine?",
+        "a": "Kilometar na sat",
+        "b": "Metar u sekundi",
+        "c": "Sati po kilometru",
+        "d": "Sekunde po metru",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Koja rijeka teče kroz Kairo?",
+        "a": "Nil",
+        "b": "Amazon",
+        "c": "Gang",
+        "d": "Misisipi",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja je aritmetička sredina brojeva 12, 18, i 24?",
+        "a": "16",
+        "b": "18",
+        "c": "20",
+        "d": "22",
+        "odgovor": "b"
+    }],
+    [{
+        "pitanje": "Koja vrsta biomolekula čini strukturu mišića?",
+        "a": "Proteini",
+        "b": "Ugljikohidrati",
+        "c": "Lipidi",
+        "d": "Nukleinske kiseline",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja zemlja ima najviše vulkana?",
+        "a": "Italija",
+        "b": "Japan",
+        "c": "Indonezija",
+        "d": "Island",
+        "odgovor": "c"
+    }, {
+        "pitanje": "Šta predstavlja skraćenica 'DNA'?",
+        "a": "Deoksiribonukleinska kiselina",
+        "b": "Disulfidna kiselina",
+        "c": "Dinitroksidna kiselina",
+        "d": "Dipeptidna kiselina",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Šta predstavlja treći Newtonov zakon?",
+        "a": "Zakon gravitacije",
+        "b": "Zakon akcije i reakcije",
+        "c": "Zakon inercije",
+        "d": "Zakon očuvanja energije",
+        "odgovor": "b"
+    }],
+    [{
+        "pitanje": "Koja enzimska supstanca omogućava replikaciju DNA?",
+        "a": "DNA polimeraza",
+        "b": "RNA polimeraza",
+        "c": "Ligaza",
+        "d": "Helikaza",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Šta je Planckova konstanta?",
+        "a": "Brzina svjetlosti",
+        "b": "Elementarni naboj",
+        "c": "Konstanta gravitacije",
+        "d": "Osnovna kvanta energije",
+        "odgovor": "d"
+    }, {
+        "pitanje": "Koja je Riemannova hipoteza?",
+        "a": "Hipoteza o nejednakosti",
+        "b": "Hipoteza o broju prim brojeva",
+        "c": "Hipoteza o kvadratnim formama",
+        "d": "Hipoteza o nulama Riemannove funkcije",
+        "odgovor": "d"
+    }, {
+        "pitanje": "Koja je najmanja nezavisna država u svijetu?",
+        "a": "Vatikan",
+        "b": "Monako",
+        "c": "San Marino",
+        "d": "Nauru",
+        "odgovor": "a"
+    }],
+    [{
+        "pitanje": "Koja vrsta RNA učestvuje u sintezi proteina?",
+        "a": "mRNA",
+        "b": "rRNA",
+        "c": "tRNA",
+        "d": "snRNA",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Šta predstavlja Heisenbergov princip neodređenosti?",
+        "a": "Odnos energije i mase",
+        "b": "Neodređenost položaja i impulsa",
+        "c": "Elektromagnetska indukcija",
+        "d": "Odnos amplitude i frekvencije",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Šta predstavlja Eulerova formula?",
+        "a": "e^(iπ) + 1 = 0",
+        "b": "√2 + √3 = √5",
+        "c": "πr^2",
+        "d": "a² + b² = c²",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja planeta ima najdužu rotaciju oko svoje ose u Sunčevom sistemu?",
+        "a": "Zemlja",
+        "b": "Venera",
+        "c": "Mars",
+        "d": "Jupiter",
+        "odgovor": "b"
+    }],
+    [{
+        "pitanje": "Koja je funkcija endoplazmatskog retikuluma u ćeliji?",
+        "a": "Sinteza proteina",
+        "b": "Proizvodnja energije",
+        "c": "Detoksikacija",
+        "d": "Transport materijala",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Šta predstavlja Schrödingerova jednačina u kvantnoj mehanici?",
+        "a": "Valnu funkciju čestice",
+        "b": "Zakon gravitacije",
+        "c": "Zakon očuvanja energije",
+        "d": "Gaussov zakon",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Kako se naziva proces pri kojem se atomi spajaju i oslobađa se energija?",
+        "a": "Fuzija",
+        "b": "Fisijski raspada",
+        "c": "Radioaktivnost",
+        "d": "Spajanje nukleusa",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koji broj predstavlja vrijednost π (pi) do prve desetice?",
+        "a": "3.141592653",
+        "b": "3.141592654",
+        "c": "3.141592655",
+        "d": "3.141592656",
+        "odgovor": "a"
+    }],
+    [{
+        "pitanje": "Šta predstavlja Le Chatelierov princip u hemiji?",
+        "a": "Brzina kemijske reakcije",
+        "b": "Ravnoteža kemijske reakcije",
+        "c": "Raspodjela elektrona",
+        "d": "Odnos masa i energije",
+        "odgovor": "b"
+    }, {
+        "pitanje": "Šta je Higgs bozon?",
+        "a": "Elementarna čestica",
+        "b": "Mjera električne otpornosti",
+        "c": "Mjera energije",
+        "d": "Sila gravitacije",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Šta predstavlja Broj Euler (e) u matematici?",
+        "a": "2.718",
+        "b": "3.142",
+        "c": "1.618",
+        "d": "2.236",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Koja rijeka teče kroz St. Petersburg?",
+        "a": "Volga",
+        "b": "Neva",
+        "c": "Dunav",
+        "d": "Sena",
+        "odgovor": "b"
+    }],
+    [{
+        "pitanje": "Šta je neriješeni problem u teoriji brojeva koji glasi: 'Da li postoji cijeli broj n koji je jednak zbiru kvadrata dva druga cijela broja?'",
+        "a": "Fermatova posljednja teorema",
+        "b": "Goldbachova konjekura",
+        "c": "Riemannova hipoteza",
+        "d": "Collatz conjecture",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Šta je enzim Rubisco i koja mu je uloga u fotosintezi?",
+        "a": "Enzim za sintezu DNK",
+        "b": "Enzim za fotosintetsku disociaciju vode",
+        "c": "Enzim za fiksaciju ugljika u Calvinov ciklus",
+        "d": "Enzim za sintezu RNA",
+        "odgovor": "c"
+    }, {
+        "pitanje": "Koja je najveća planina izvan našeg planeta Sunčevog sistema?",
+        "a": "Olympus Mons (Mars)",
+        "b": "Mount Everest (Zemlja)",
+        "c": "Mauna Kea (Zemlja)",
+        "d": "Mons Huygens (Mjesec)",
+        "odgovor": "a"
+    }, {
+        "pitanje": "Šta predstavlja Grand Unified Theory (GUT) u fizici čestica?",
+        "a": "Teorija svega",
+        "b": "Teorija kvantne gravitacije",
+        "c": "Teorija jedinstvenih polja",
+        "d": "Teorija relativnosti",
+        "odgovor": "c"
+    }]
+];
+
+var nagrade=["0 KM", "100 KM", "200 KM", "300 KM", "500 KM", "1,000 KM",
+            "2,000KM", "4000 KM", "8,000 KM", "16,000KM", "32,000 KM", "64,000 KM",
+            "125,000 KM", "250,000 KM", "500,000 KM", "1 MILION"];
+
+var brojTacnih=0;
+var pitanje;
+var pomoc50;
+var pomocF;
+var pomocP;
+var i=-1;
+function dajPitanje(){
+    i++;
+    var j = Math.floor(Math.random() * 4);
+    return pitanja[i][j];  
+}
+
+var kviz_pitanje = document.getElementById("kviz-pitanje");
+var kviz_odgovor1 = document.getElementById("kviz-odgovor1");
+var kviz_odgovor2 = document.getElementById("kviz-odgovor2");
+var button1, button2, button3, button4;
+
+function prikaz(){
+    pitanje= dajPitanje();
+    kviz_pitanje.innerHTML= '';
+    var pElement = document.createElement("p");
+    pElement.innerHTML = pitanje["pitanje"];
+    pElement.style.fontWeight = "bold";
+    pElement.style.color = "white";
+    kviz_pitanje.appendChild(pElement);
+
+
+    button1 = document.createElement("button");
+    button1.textContent = pitanje["a"];
+    button1.classList.add("btn", "btn-light", "dugme1", "dugme");
+
+    button2 = document.createElement("button");
+    button2.textContent = pitanje["b"];
+    button2.classList.add("btn", "btn-light", "dugme1", "dugme");
+
+    button3 = document.createElement("button");
+    button3.textContent = pitanje["c"];
+    button3.classList.add("btn", "btn-light", "dugme1", "dugme");
+
+    button4 = document.createElement("button");
+    button4.textContent = pitanje["d"];
+    button4.classList.add("btn", "btn-light", "dugme1", "dugme");
+
+    kviz_odgovor1.innerHTML='';
+    kviz_odgovor2.innerHTML='';
+    kviz_odgovor1.appendChild(button1);
+    kviz_odgovor1.appendChild(button2);
+    kviz_odgovor2.appendChild(button3);
+    kviz_odgovor2.appendChild(button4);
+    
+    var pomocPola = document.getElementById('lifeline50');
+
+    pomocPola.addEventListener('click', function() {
+        if(pomoc50==0) return;
+        if(pitanje["odgovor"]== "a"){
+            button2.style.display = "none";
+            button3.style.display = "none";
+            
+        }
+        else if(pitanje["odgovor"]== "b"){
+            button1.style.display = "none";
+            button3.style.display = "none";
+        }   
+        else if(pitanje["odgovor"]== "c"){
+            button2.style.display = "none";
+            button4.style.display = "none";
+            
+        }
+        else{
+            button2.style.display = "none";
+            button1.style.display = "none";
+           
+        }
+        pomoc50--;
+    });
+
+    var pomocFrenda = document.getElementById('lifelineFriend');
+
+    pomocFrenda.addEventListener('click', function() {
+        if(pomocF==0) return;
+        if(pitanje["odgovor"]== "a"){
+            alert("Prijatelj je siguran da je '" + pitanje["a"] + "' odgovor sa sigurnošću 68%!");
+        }
+        else if(pitanje["odgovor"]== "b"){
+            alert("Prijatelj je siguran da je '" + pitanje["b"] + "' odgovor sa sigurnošću 80%!");
+        }
+        else if(pitanje["odgovor"]== "c"){
+            alert("Prijatelj je siguran da je '" + pitanje["c"] + "' odgovor sa sigurnošću 70%!");
+        }
+        else{
+            alert("Prijatelj je siguran da je '" + pitanje["d"] + "' odgovor sa sigurnošću 90%!");
+        }
+        pomocF--;
+    });
+
+    var pomocPublike = document.getElementById('lifelinePublika');
+
+    pomocPublike.addEventListener('click', function() {
+        if(pomocP==0) return;
+        if(pitanje["odgovor"]== "a"){
+            alert("Publika je glasala sljedeće:\n" + pitanje["a"] + " -> 76%\n" +
+            pitanje["b"] + " -> 16%\n" +
+            pitanje["c"] + " -> 5%\n" +
+            pitanje["b"] + " -> 3%\n");
+        }
+        else if(pitanje["odgovor"]== "b"){
+            alert("Publika je glasala sljedeće:\n" + pitanje["a"] + " -> 25%\n" +
+            pitanje["b"] + " -> 59%\n" +
+            pitanje["c"] + " -> 17%\n" +
+            pitanje["b"] + " -> 24%\n");
+        }
+        else if(pitanje["odgovor"]== "c"){
+            alert("Publika je glasala sljedeće:\n" + pitanje["a"] + " -> 16%\n" +
+            pitanje["b"] + " -> 8%\n" +
+            pitanje["c"] + " -> 67%\n" +
+            pitanje["b"] + " -> 9%\n");
+        }
+        else{
+            alert("Publika je glasala sljedeće:\n" + pitanje["a"] + " -> 7%\n" +
+            pitanje["b"] + " -> 15%\n" +
+            pitanje["c"] + " -> 6%\n" +
+            pitanje["b"] + " -> 72%\n");
+        }
+        pomocP--;
+    });
+
+    button1.addEventListener("click", function () {checkAnswer(pitanje, 'a')});
+    button2.addEventListener("click", function () {checkAnswer(pitanje, 'b')});
+    button3.addEventListener("click", function () {checkAnswer(pitanje, 'c')});
+    button4.addEventListener("click", function () {checkAnswer(pitanje, 'd')});
+}
+
+function milijunas(){
+    pomoc50=1;
+    pomocF=1;
+    pomocP=1;
+    prikaz();
+}
+
+function checkAnswer(pitanje, selectedOption) {
+    if (pitanje["odgovor"] === selectedOption) {
+        brojTacnih++;
+
+        var progressBar = document.getElementById('myProgressBar');
+        progressBar.style.width = `${(brojTacnih / 15) * 100}%`;
+        progressBar.innerText = nagrade[brojTacnih];
+        if(brojTacnih<15) prikaz();
+        else{
+
+            kviz_pitanje.innerHTML = '🎉Čestitke, osvojili ste ' + nagrade[brojTacnih] + '🎉';
+            kviz_pitanje.style.color = "white";
+            kviz_odgovor1.innerHTML='';
+            kviz_odgovor2.innerHTML='';
+        }
+    } 
+    else {
+
+        kviz_pitanje.innerHTML = 'Osvojili ste ' + nagrade[brojTacnih];
+        kviz_pitanje.style.color = "white";
+        kviz_odgovor1.innerHTML='';
+        kviz_odgovor2.innerHTML='';       
+    }
+}
+
+
